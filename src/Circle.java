620d8490f11a6cd553a105035a62a452386f2890
andresantosr <andresanro03@gmail.com>
2023-06-24 18:43:43 -0300

    ovais concluidos

--- src/Circle.java
+++ src/Circle.java
@@ -1,16 +1,40 @@
 import java.awt.*;
 import java.util.StringTokenizer;
 
-public class Circle extends Figura {
+public class Circle extends Figure {
     protected Point p1,p2;
     protected int diameter;
     
-    public Circle(int x1, int y1, int x2, int y2, Color cor) {
-        super(cor);
-        this.p1 = new Point(x1, y1);
-        this.p2 = new Point(x2, y2);
+    //Default constructor
+    public Circle(Point p1, Point p2) {
+        super();
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
+    //Constructor with a pre-selected color
+    public Circle(Point p1, Point p2, int diameter, Color cor) {
+        super(cor);
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    //Getters
+    public Point getP1()
+    {
+        return this.p1;
+    }
+
+    public Point getP2()
+    {
+        return this.p2;
+    }
+
+    public int getDiameter(){
+        return this.diameter;
+    }
+
+    //Setters
     public void setP1(int x, int y)
     {
         this.p1.setX(x);
@@ -23,104 +47,54 @@ public class Circle extends Figura {
         this.p1.setY(y);
     }
 
-    public Point getP1()
-    {
-        return this.p1;
+    public void setDiameter(int diameter){
+        this.diameter = diameter;
     }
-
-    public Point getP2()
-    {
-        return this.p2;
-    }
-
     
+    //toString
+    @Override
+    public String toString() {
+        return  super.toString() + 
+                "p1: " + p1 + "\n" + 
+                "p2: " + p2 + "\n" +
+                "diameter: " + diameter;
+    }
 
-    private void calculateSize()
+    //Calculate the dimensions of the circle
+    private void calculateDimensions()
     {
         int deltaX, deltaY;
-
+        
+        //calculating deltaX
         deltaX = this.p1.getX() - this.p2.getX();
-
-        if(deltaX < 0)
-            deltaX = -deltaX;
-
+        if(deltaX < 0) {
+            deltaX = -deltaX; //modulo
+        }
+        //calculating deltaX
         deltaY = this.p1.getY() - this.p2.getY();
-
-        if(deltaY < 0)
+        if(deltaY < 0) {
             deltaY = -deltaY;
-
-        if(deltaX >= deltaY) //vejo se a distancia entre os dois pontos é maior no eixo X ou no eixo Y
+        }
+        if(deltaX >= deltaY) //check which delta is bigger
         {
-            this.diametro = deltaX;                              //sendo no eixo x:
+            this.diameter = deltaX;                              
                                                                 
         }
         else 
         {
-            this.diametro = deltaY; 
+            this.diameter = deltaY; 
         }
     }
 
-    public String toString()
-    {
-        return "c:" +
-               this.p1.getX() +
-               ":" +
-               this.p1.getY() +
-               ":" +
-               this.p2.getX() +
-               ":" +
-               this.p2.getY() +
-               ":" +
-               this.getColor().getRed() +
-               ":" +
-               this.getColor().getGreen() +
-               ":" +
-               this.getColor().getBlue();
-    }
-
-    //outros metodos
+    //Draw the circle
     @Override
-    public Object clone()
-    {
-        Circle clone = null;
-        try
-        {
-            clone = new Circle(this);
-        }
-        catch(Exception e)
-        {}      
-        return clone;
+    public void draw(Graphics g, Color fc){
+        g.setColor(this.color); //contour color
+        calculateDimensions();
+        g.drawOval(p1.getX(), p1.getY(), //start point
+                   diameter, diameter);
+        g.setColor(fc); //filling color
+        g.fillOval(p1.getX()+1, p1.getY()+1, //adding  1 to starting point to avoid overdrawing
+                   diameter, diameter);
     }
-
-    public Circle(Circle modelo) throws Exception{
-        if(modelo==null){
-            throw new Exception("nao pode ser nulo");
-        }
-        this.p1 = modelo.p1;
-        this.p2 = modelo.p2;
-        this.diameter = modelo.diameter;
-        this.diameter = modelo.diameter;
-
-    }
-    
-
-    @Override
-    public int hashCode()
-    {
-        int ret = 111;
-        ret = ret *5 + this.p1.hashCode();
-        ret = ret * 5 + this.p2.hashCode();
-        ret = ret * 5 + new Integer(this.diameter).hashCode();
-        ret = ret * 5 + new Integer(this.diameter).hashCode();
-        if(ret<0)
-            ret = -ret;
-
-        return ret;
-    }
-
-
-
-
-
-
 }
--- src/Elipse.java
+++ src/Elipse.java
@@ -1,3 +1,112 @@
-public class Elipse {
+import java.awt.*;
+
+public class Ellipse extends Figure {
+    Point p1, p2, p3;
+    int height = 0, width = 0;
+    Color color;
+
+    //DEfault constructor
+    public Ellipse(Point p1, Point p2, Point p3, int height, int width)
+    {
+        super();
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.height = height;
+        this.width = width;
+
+    }
+
+    //constructor with a pre-selected color
+    public Ellipse(Point p1, Point p2, Point p3, int height, int width, Color color) {
+        super(color);
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.height = height;
+        this.width = width;
+    }
     
+    //Getters
+    public Point getP1() {
+        return this.p1;
+    }
+
+    public Point getP2() {
+        return this.p2;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    //Setters
+    public void setP1(int x, int y)
+    {
+        this.p1.setX(x);
+        this.p1.setY(y);
+    }
+
+    public void setP2(int x, int y)
+    {
+        this.p2.setX(x);
+        this.p2.setY(y);
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return  super.toString() + 
+                "p1: " + p1 + "\n" + 
+                "p2: " + p2 + "\n" +
+                "height: " + height + "\n" +
+                "width: " + width + "\n" +
+                "color: " + color;   
+        }
+   
+    private void calculateDimensions() {
+        int deltaX, deltaY;
+        
+        //calculate deltaX
+        deltaX = this.p1.getX() - this.p2.getX(); 
+        if(deltaX < 0)
+            deltaX = -deltaX; //module
+        
+        //calculate deltaY
+        deltaY = this.p1.getY() - this.p2.getY(); 
+        if(deltaY < 0)
+            deltaY = -deltaY; //module
+
+        if(deltaX >= deltaY) //check which delta is bigger
+            if(this.width == 0)
+                this.width = deltaX;
+            else
+                this.height = deltaX;
+        else
+            if(this.height == 0)
+                this.height = deltaY;
+            else
+                this.width = deltaY;
+    }
+
+    public void Draw(Graphics g,Color c) {
+        g.setColor(this.color);
+        calculateDimensions();  
+        g.drawOval(this.p1.getX(), this.p1.getY(), width, height); 
+        g.setColor(c);//set color to fill in the ellipse
+        g.fillOval(this.p1.getX(), this.p1.getY(), width, height);
+    }
+        
 }

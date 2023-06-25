a87f5b277121c3b39b0f31b06de35d59c8471539
ra230797 <m230797@dac.unicamp.br>
2023-06-25 17:24:46 -0300

    fixing class constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,4 @@
-public class Constants {
+public abstract class Constants {
     public static String[] figureNames = {"Point", "Line", "Circle", "Ellipse", "Square", "Rectangle", "Outside", "Inside", "Open", "Save"};
     public static final int NUMBER_OF_BUTTONS = figureNames.length;
 }
